f19d2506165cc7d8465a29135e3e6a001beb98dd
tloukit <83870677+tloukit@users.noreply.github.com>
2022-12-21 09:39:24 +0100

    initial commit

---
+++ spring-boot-todo-application/src/main/java/com/tloukit/springboottodoapplication/SpringBootTodoApplication.java
@@ -0,0 +1,13 @@
+package com.tloukit.springboottodoapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTodoApplication.class, args);
+	}
+
+}
