d442098ea62cdda47f777ec418a6a208de4d44b9
tloukit <83870677+tloukit@users.noreply.github.com>
2022-12-21 11:18:45 +0100

    Add TodoItemController and index.html template

---
+++ spring-boot-todo-application/src/main/java/com/tloukit/springboottodoapplication/controllers/TodoItemController.java
@@ -0,0 +1,20 @@
+package com.tloukit.springboottodoapplication.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class TodoItemController {
+	private final Logger logger = LoggerFactory.getLogger(TodoItemController.class);
+	
+	@GetMapping("/")
+	public ModelAndView index() {
+		logger.debug("request to get the index page");
+		ModelAndView modelAndView = new ModelAndView("index");
+		return modelAndView;
+	}
+
+}
